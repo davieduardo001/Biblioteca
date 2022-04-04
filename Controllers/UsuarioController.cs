57cc4831be46404f8f19aca248dd370aa46315ee
davieduardo001 <davieduardocosta01@gmail.com>
2022-04-04 19:44:29 -0300

    Função de exclusão incluida

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,11 +1,19 @@
 using Biblioteca.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System;
 
 namespace Biblioteca.Controllers
 {
     public class UsuarioController : Controller
     {
+
+        //Cadastrar Usuarios
         public IActionResult Cadastro()
         {
             return View();
@@ -27,12 +35,36 @@ namespace Biblioteca.Controllers
             return RedirectToAction("Listagem");
         }
 
-        
+       //Listar Usuarios 
         public IActionResult Listagem()
         {
             UsuarioService us = new UsuarioService();
             List<Usuario> u = us.Listar();
             return View(u);
+        }
+
+        //Editar Usuarios
+        public IActionResult Edicao(int idU)
+        {
+            UsuarioService us = new UsuarioService();
+            Usuario u = us.EncontrarId(idU);
+            return View(u);
+        }
+        [HttpPost]
+        public IActionResult Edicao(Usuario u)
+        {
+            UsuarioService us = new UsuarioService();
+            us.Editar(u);
+            return RedirectToAction("Listagem");
+        }
+
+        //Excluir Usuarios
+        public IActionResult Exclusao(Usuario u)
+        {
+            Autenticacao.CheckLogin(this);
+            UsuarioService us = new UsuarioService();
+            us.Excluir(u);
+            return View();
         }
     }
 }
--- Models/UsuarioService.cs
+++ Models/UsuarioService.cs
@@ -10,6 +10,7 @@ namespace Biblioteca.Models
         
         private const string _strConection = "Database=Biblioteca;Data Source=localhost;User Id=root;";
 
+        // Atualizar usuario
         public void Inserir(Usuario u)
         {
             using(BibliotecaContext bc = new BibliotecaContext())
@@ -18,7 +19,6 @@ namespace Biblioteca.Models
                 bc.SaveChanges();
             }
         }
-
         public void Atualizar(Usuario u)
         {
             using(BibliotecaContext bc = new BibliotecaContext())
@@ -58,5 +58,69 @@ namespace Biblioteca.Models
             return list;
         }
 
+
+        // Alterar Usuario
+            //Encontrar Id
+        public Usuario EncontrarId(int Id)
+        {
+            MySqlConnection connection = new MySqlConnection(_strConection);
+            connection.Open();
+
+            string sql = "SELECT * FROM Usuarios WHERE Id=@Id";
+
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            
+            command.Parameters.AddWithValue("@Id", Id);
+
+            MySqlDataReader reader = command.ExecuteReader();
+
+            Usuario UsuarioEncontrado = new Usuario();
+
+            if (reader.Read())
+            {
+                UsuarioEncontrado.Id = reader.GetInt32("Id");
+
+                if(!reader.IsDBNull(reader.GetOrdinal("Login")))
+                    UsuarioEncontrado.Login = reader.GetString("Login");
+
+                if(!reader.IsDBNull(reader.GetOrdinal("Senha")))
+                    UsuarioEncontrado.Senha = reader.GetString("Senha");
+            }
+
+            connection.Close();
+            return UsuarioEncontrado;
+        }
+            //Editar noticas
+        public void Editar(Usuario u)
+        {
+            MySqlConnection connection = new MySqlConnection(_strConection);
+            connection.Open();
+
+            string sql = "UPDATE Usuarios SET Login=@Login, Senha=@Senha";
+
+            MySqlCommand command = new MySqlCommand(sql, connection);
+
+            command.Parameters.AddWithValue("@Login", u.Login);
+            command.Parameters.AddWithValue("@Senha", u.Senha);
+
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+    
+        //Excluir Usuario
+        public void Excluir(Usuario u)
+       {
+           MySqlConnection connection = new MySqlConnection(_strConection);
+           connection.Open();
+
+           string sql = "DELETE FROM Usuarios WHERE Id = @Id";
+
+           MySqlCommand command = new MySqlCommand(sql, connection);
+
+           command.Parameters.AddWithValue("@Id", u.Id);
+
+           command.ExecuteNonQuery();
+           connection.Close();
+       }
     }
 }
